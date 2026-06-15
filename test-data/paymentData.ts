9dae070ba71d111a1d24c77e71cde2379e436218
Git-Raini <rainidesai20@gmail.com>
2026-06-15 23:02:19 +0530

    Add payment test data

---
+++ test-data/paymentData.ts
@@ -0,0 +1,13 @@
+export const paymentData = {
+  validCard: {
+    number: "4111111111111111",
+    expiry: "12/30",
+    cvv: "123",
+  },
+
+  invalidCard: {
+    number: "4000000000000002",
+    expiry: "12/30",
+    cvv: "123",
+  },
+};
